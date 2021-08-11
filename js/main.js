fd6ebd917ba0418f8f4c759b954288affe53c98b
Michael Uzan <looply3@gmail.com>
2021-08-11 18:07:17 +0300

    1 can choose photo 2 can write 3 can drag text

--- js/main.js
+++ js/main.js
@@ -1,24 +1,72 @@
 'use strict'
-var gMeme = {
-    selectedImgId: 0,
-    selectedLineIdx: 0,
-    lines: [
-        {
-            txt: '',
-            size: 20,
-            align: 'left',
-            color: 'red'
-        }
-    ]
-}
+
 var gCtx;
 var gCanvas;
+var gStartPos;
+const gTouchEvs = ['touchstart', 'touchmove', 'touchend']
 
 function onInit() {
     gCanvas = document.getElementById('canvas');
     gCtx = gCanvas.getContext('2d');
     drawText('Choose a photo', 150, 150)
+    addListeners()
+}
 
+function addListeners() {
+    // Mouse 
+    gCanvas.addEventListener('mousedown', onDown)
+    gCanvas.addEventListener('mousemove', onMove)
+    gCanvas.addEventListener('mouseup', onUp)
+    // Touch
+    gCanvas.addEventListener('touchstart', onDown)
+    gCanvas.addEventListener('touchmove', onMove)
+    gCanvas.addEventListener('touchend', onUp)
+}
+
+function onDown(ev) {
+    const clickedPos = getEvPos(ev)
+    console.log('pos', clickedPos)
+    if (!isLineClicked(clickedPos)) return
+    gStartPos = clickedPos
+    document.body.style.cursor = 'grabbing'
+    // document.querySelector('body').style.cursor = 'grab'
+}
+
+function onMove(ev) {
+    const line = getDragLine();
+    if (!line) return;
+    if (line.isDrag) {
+        const pos = getEvPos(ev)
+        const dx = pos.x - gStartPos.x
+        const dy = pos.y - gStartPos.y
+        moveLine(dx, dy)
+        gStartPos = pos
+        renderCanvas()
+    }
+}
+
+function onUp() {
+    setFlaseLineDrag()
+    document.body.style.cursor = 'auto'
+    // document.querySelector('body').style.cursor = 'auto'
+
+}
+
+
+function getEvPos(ev) {
+    var pos = {
+        x: ev.offsetX,
+        y: ev.offsetY
+    }
+    if (gTouchEvs.includes(ev.type)) {
+        ev.preventDefault()
+        ev = ev.changedTouches[0]
+        pos = {
+            x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+            y: ev.pageY - ev.target.offsetTop - ev.target.clientTop
+        }
+    }
+    return pos
 }
 
 function drawImgFromLocal(imgUrl) {
@@ -32,40 +80,42 @@ function drawImgFromLocal(imgUrl) {
 }
 
 function onSelectImage(photoId) {
-    var currImg = getSelectedImage(photoId) // {id url keywords: [])
-    gMeme.selectedImgId = currImg.id
+    resetMeme()
+    setSelectedImg(photoId)
     renderCanvas()
 }
 
-function drawText(txt, x, y) {
-    gCtx.font = '48px serif';
+function drawText(txt, x, y, size = 100) {
     gCtx.lineWidth = 2
     gCtx.strokeStyle = 'brown'
     gCtx.fillStyle = 'white'
-    gCtx.font = '40px Arial'
+    gCtx.font = `${size}px Arial`
     gCtx.fillText(txt, x, y)
     gCtx.strokeText(txt, x, y)
 }
 
 function onType(txt, lineIdx) {
+    if (!isCanvas()) return
     console.log('rendering on Canvas: ', txt);
-    gMeme.lines[lineIdx].txt = txt;
+    updateTxtLine(txt, lineIdx)
     renderCanvas()
 }
 
 function renderCanvas() {
-    var currImg = getSelectedImage(gMeme.selectedImgId) // {id url keywords: [])
+    var currImg = getSelectedImage() // {id url keywords: [])
     drawImgFromLocal(currImg.url)
     drawLines();
 }
 
 function drawLines() {
-    gMeme.lines.forEach(line => {
-        drawText(line.txt, 150, 150)
+    var meme = getMeme();
+    meme.lines.forEach(line => {
+        drawText(line.txt, line.position.x, line.position.y, line.fontSize)
     });
 }
 
-// txt: 'Hello',
-// size: 20,
-// align: 'left',
-// color: 'red'
+function onPlusFont(diff) {
+    if (!isCanvas()) return
+    plusFont(diff)
+    renderCanvas()
+}
--- js/service/memeService.js
+++ js/service/memeService.js
@@ -1,26 +1,105 @@
 'use strict'
 
+var gMeme = createMeme();
 const gImgs = [
     {
-        id: 0,
+        id: 1,
         url: 'img/1.jpg',
         keywords: []
     },
     {
-        id: 1,
+        id: 2,
         url: 'img/2.jpg',
         keywords: []
     },
     {
-        id: 2,
+        id: 3,
         url: 'img/3.jpg',
         keywords: []
     }
 ];
 
-function getSelectedImage(photoId) {
+function createMeme() {
+    return {
+        selectedImgId: null,
+        selectedLineIdx: null,
+        isDrag: false,
+        lines: [
+            {
+                txt: '',
+                fontSize: 45,
+                align: null,
+                color: null,
+                position: { x: 150, y: 150 },
+                isDrag: false
+            }
+        ]
+    }
+}
+
+function getSelectedImage() {
     var photoIdx = gImgs.findIndex(function (img) {
-        return photoId === img.id
+        return gMeme.selectedImgId === img.id
     })
     return gImgs[photoIdx];
 }
+
+function setSelectedImg(photoId) {
+    gMeme.selectedImgId = photoId
+}
+
+function updateTxtLine(txt, lineIdx) {
+    gMeme.selectedLineIdx = lineIdx
+    gMeme.lines[lineIdx].txt = txt;
+}
+
+function getMeme() {
+    return gMeme;
+}
+
+function resetMeme() {
+    gMeme = createMeme()
+}
+
+function isCanvas() {
+    return gMeme.selectedImgId
+}
+
+function plusFont(diff) {
+    var fontSize = gMeme.lines[gMeme.selectedLineIdx].fontSize
+    if (fontSize === 0 || fontSize === 150) return
+    gMeme.lines[gMeme.selectedLineIdx].fontSize += diff
+}
+
+function isLineClicked(clickedPos) {
+
+    var lineIdx = gMeme.lines.findIndex(function (line) {
+        console.log('fontSize', line.fontSize)
+        console.log('txt.length', line.txt.length)
+        return ((clickedPos.x > line.position.x && clickedPos.x < line.position.x + ((line.fontSize / 2) * line.txt.length)) &&
+            (clickedPos.y < line.position.y) && (clickedPos.y > (line.position.y - line.fontSize)))
+    })
+    if (lineIdx === -1) return false;
+    // setFlaseLineDrag(true, lineIdx)
+    gMeme.selectedLineIdx = lineIdx;
+    gMeme.lines[lineIdx].isDrag = true;
+    gMeme.isDrag = true;
+    return true
+}
+
+function getDragLine() {
+    return gMeme.lines[gMeme.selectedLineIdx]
+}
+
+function moveLine(dx, dy) {
+    gMeme.lines[gMeme.selectedLineIdx].position.x += dx
+    gMeme.lines[gMeme.selectedLineIdx].position.y += dy
+}
+
+function setFlaseLineDrag() {
+    if (!gMeme.isDrag) return
+    gMeme.lines[gMeme.selectedLineIdx].isDrag = false;
+    // gMeme.selectedLineIdx = null;
+    gMeme.isDrag = false;
+}
+
